73a27c4d99323ae3473116c071fb89c31da8b56a
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2023-05-13 15:12:20 +1200

    v.0.0.0.5 minimap
    Update mini map. Mini map now is not rotating with the player. It's not displaying the item enemy yet.

--- Capybara_time/Assets/Scripts/MinimapScript.cs
+++ Capybara_time/Assets/Scripts/MinimapScript.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class MinimapScript : MonoBehaviour
 {
-    public Transform playerTransform;
-    public float offset_y = 10f;
+    [SerializeField] private Transform playerTransform;
 
-    void Update()
+    private void LateUpdate()
     {
-        transform.position = playerTransform.position + Vector3.up * offset_y;
+        // Get the position of the player character
+        Vector3 playerPosition = playerTransform.position;
+
+        // Set the position of the minimap camera to the same x and z coordinates as the player character, but keep the y coordinate of the camera
+        transform.position = new Vector3(playerPosition.x, transform.position.y, playerPosition.z);
+
+        // Reset the rotation of the minimap camera so it doesn't rotate with the player character
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
     }
 }
 
